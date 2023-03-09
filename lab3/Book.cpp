73549fb8ef066b31c7e7d59af7b92b3c77b044dd
Chibby332 <dead777boy@gmail.com>
2023-03-09 18:17:36 +0700

    Добавьте файлы проекта.

---
+++ lab3/Book.cpp
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <iostream>
+#include "Book.h"
+using namespace std;
+
+int main()
+{
+    
+}
+
+void DemoBook() {
+    Book book;
+    book.Name = "Harry Potter I";
+    book.Year = 2002;
+    book.Page = 399;
+    book.Autors[0] = "JK Rowling";
+    book.Autors[1] = "Matvey Chibrkov";
+    book.Autors[2] = "Matvey Chibrkov";
+    book.AutorsCount = 3;
+}
---
+++ lab3/Book.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+struct Book {
+	string Name;
+	int Year;
+	int Page;
+	string Autors[10];
+	int AutorsCount;
+};
+void DemoBook();
